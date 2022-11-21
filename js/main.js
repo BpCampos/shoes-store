71148a1ae19792ccb36f92d03699a45095d89f2a
Bruno Campos <campos.bruno98@gmail.com>
2022-11-21 13:58:17 -0300

    added form verification

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 console.log("rodando js");
 
 window.onload = () => {
-  //focus
-  let fieldName = document.querySelector("#nome");
+  let form = document.querySelector(".trabalhe-conosco");
 
-  fieldName.addEventListener("focus", () => {
-    fieldName.style.border = "2px solid green";
+  form.addEventListener("submit", (event) => {
+    let errors = [];
+
+    let inputNome = document.querySelector("#nome");
+    let inputEmail = document.querySelector("#email");
+    let inputTel = document.querySelector("#tel");
+
+    if (inputNome.value == "") {
+      errors.push("O campo nome não pode estar vazio");
+    }
+
+    if (inputEmail.value == "") {
+      errors.push("O campo email não pode estar vazio");
+    }
+
+    if (inputTel.value == "") {
+      errors.push("O campo telefone não pode estar vazio");
+    }
+
+    if (errors.length > 0) {
+      event.preventDefault();
+      console.log(errors);
+    }
   });
 };
